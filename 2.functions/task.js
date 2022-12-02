4d95d9416ccaba0f23f78d5a45a9b086032487e7
MadDolph <kurbatov@it-matika.pro>
2022-12-03 01:47:03 +0300

    1 task complete

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,5 +1,25 @@
 function getArrayParams(...arr) {
 
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
+  let avg;
+
+  for (i = 0; i < arr.length; i++) {
+
+    if(arr[i] > max) max = arr[i];
+
+    if(arr[i] < min) min = arr[i];
+
+    sum += arr[i];
+
+
+  }
+
+  avg = sum / arr.length;
+
+  avg = + avg.toFixed(2);
+
   return { min: min, max: max, avg: avg };
 }
 
