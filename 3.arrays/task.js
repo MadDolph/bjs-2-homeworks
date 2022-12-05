1e8587e9fcc5c7134e28f34833df437fd33cde64
MadDolph <kurbatov@it-matika.pro>
2022-12-06 02:12:59 +0300

    Chapter Arrays: homework is complete

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,20 @@
 function compareArrays(arr1, arr2) {
-  
+  const compare = (arr1.length == arr2.length) && arr1.every((el, i) => el == arr2[i]);
+
+  return compare;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  const result = users.filter(item => item.gender == gender).reduce(function(accum, item, index, arr) {
+    if(arr.length !== (index + 1)) {
+        accum += item.age;
+      } else {
+        console.log(accum);
+        accum += item.age;
+        accum = accum / arr.length;
+      }
+    return accum;
+  }, 0) 
+
+  return result;
 }
