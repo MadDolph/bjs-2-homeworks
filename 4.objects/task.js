643604c7f67f6691e117bac61d446fdfddc3443f
MadDolph <kurbatov@it-matika.pro>
2022-12-10 17:24:35 +0300

    Half

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,16 +1,22 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];  
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  for(let mark of marks) {
+    this.marks.push(mark);
+  }
 }
 
 Student.prototype.getAverage = function () {
+   
   
 }
 
