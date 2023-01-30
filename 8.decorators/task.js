0be52254ff832be85f24ecc528da7ebd592d3eff
MadDolph <kurbatov@it-matika.pro>
2023-01-31 02:04:11 +0300

    Headroll

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,33 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+  let cache = [];
+  function wrapper(...args) {
+    const hash = md5(args);
+    let objIndex = cache.findIndex((item) => item.hash == hash);
+    if(objIndex != -1) {
+        console.log("Из кэша: " + cache[objIndex].value);
+        return ("Из кэша: " + cache[objIndex].value);
+    }
+  let result = func(...args);
+  cache.push({
+     hash: hash, 
+     value: result
+  })
+  if(cache.length > 5) {
+    cache.shift();
+  }
+  console.log("Вычисляем: " + result);
+  return ("Вычисляем: " + result);
+  }
+  return wrapper;
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
   
+  function wrapper(...args) {
+    setTimeout(() => func(...args), delay);
+ }
+ 
+ return wrapper;
 }
